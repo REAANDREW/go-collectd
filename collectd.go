0ad3c44aefb279a20a5644e5a4e68adee0cd8097
REAANDREW <email@andrewrea.co.uk>
2014-12-29 09:06:03 +0000

    refactored the package code into a separate file and updated the imports

---
+++ collectd.go
@@ -0,0 +1,103 @@
+package collectd
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+)
+
+const (
+	HOSTNAME        uint16 = 0x0000
+	HIGH_DEF_TIME   uint16 = 0x0008
+	PLUGIN          uint16 = 0x0002
+	PLUGIN_INSTANCE uint16 = 0x0003
+)
+
+type Header struct {
+	Type   uint16
+	Length uint16
+}
+
+type StringPart struct {
+	Header  Header
+	Content string
+}
+
+type NumericPart struct {
+	Header  Header
+	Content int64
+}
+
+var parsers map[uint16]parsePart
+
+func init() {
+	parsers = map[uint16]parsePart{
+		HOSTNAME:        parseStringPart,
+		HIGH_DEF_TIME:   parseHighDefNumericPart,
+		PLUGIN:          parseStringPart,
+		PLUGIN_INSTANCE: parseStringPart,
+	}
+}
+
+type parsePart func(header Header, buffer *bytes.Buffer) (part interface{}, err error)
+
+func parseHeader(buffer *bytes.Buffer) (header Header, err error) {
+	var partType uint16
+	var partLength uint16
+
+	err = binary.Read(buffer, binary.BigEndian, &partType)
+	if err != nil {
+		return Header{}, err
+	}
+
+	err = binary.Read(buffer, binary.BigEndian, &partLength)
+	if err != nil {
+		return Header{}, err
+	}
+	return Header{partType, partLength}, nil
+}
+
+func parseStringPart(header Header, buffer *bytes.Buffer) (part interface{}, err error) {
+	contentBytes := buffer.Next(int(header.Length - 4))
+	//Trim the null terminating byte from the string
+	content := string(contentBytes[0 : len(contentBytes)-1])
+	return StringPart{header, content}, nil
+}
+
+func parseNumericPart(header Header, buffer *bytes.Buffer) (part interface{}, err error) {
+	var content int64
+	err = binary.Read(buffer, binary.BigEndian, &content)
+	if err != nil {
+		return NumericPart{}, err
+	}
+	return NumericPart{header, content}, nil
+}
+
+func parseHighDefNumericPart(header Header, buffer *bytes.Buffer) (part interface{}, err error) {
+	parsedPart, err := parseNumericPart(header, buffer)
+	numericPart := parsedPart.(NumericPart)
+	if err != nil {
+		return NumericPart{}, err
+	}
+	return NumericPart{numericPart.Header, numericPart.Content >> 30}, nil
+}
+
+func parseParts(buffer *bytes.Buffer) []interface{} {
+	var partsSlice []interface{}
+	for buffer.Len() > 0 {
+		header, err := parseHeader(buffer)
+		if err != nil {
+			fmt.Errorf("err encountered %v", err)
+		}
+		if parser, ok := parsers[header.Type]; ok {
+			part, err := parser(header, buffer)
+			if err != nil {
+				fmt.Errorf("err encountered %v", err)
+			}
+			partsSlice = append(partsSlice, part)
+		} else {
+			buffer.Next(int(header.Length - 4))
+		}
+	}
+	return partsSlice
+}
--- collectd_test.go
+++ collectd_test.go
@@ -2,7 +2,6 @@ package collectd
 
 import (
 	"bytes"
-	"encoding/binary"
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
@@ -10,40 +9,8 @@ import (
 	"testing"
 )
 
-const (
-	HOSTNAME        uint16 = 0x0000
-	HIGH_DEF_TIME   uint16 = 0x0008
-	PLUGIN          uint16 = 0x0002
-	PLUGIN_INSTANCE uint16 = 0x0003
-)
-
-type Header struct {
-	Type   uint16
-	Length uint16
-}
-
-type StringPart struct {
-	Header  Header
-	Content string
-}
-
-type NumericPart struct {
-	Header  Header
-	Content int64
-}
-
 var packetBytes []byte
 var err error
-var parsers map[uint16]ParsePart
-
-func init() {
-	parsers = map[uint16]ParsePart{
-		HOSTNAME:        ParseStringPart,
-		HIGH_DEF_TIME:   ParseHighDefNumericPart,
-		PLUGIN:          ParseStringPart,
-		PLUGIN_INSTANCE: ParseStringPart,
-	}
-}
 
 func TestMain(m *testing.M) {
 	packetBytes, err = ioutil.ReadFile("cpu_disk_packet_5.4.0.dat")
@@ -52,69 +19,6 @@ func TestMain(m *testing.M) {
 	}
 
 	os.Exit(m.Run())
-}
-
-type ParsePart func(header Header, buffer *bytes.Buffer) (part interface{}, err error)
-
-func ParseHeader(buffer *bytes.Buffer) (header Header, err error) {
-	var partType uint16
-	var partLength uint16
-
-	err = binary.Read(buffer, binary.BigEndian, &partType)
-	if err != nil {
-		return Header{}, err
-	}
-
-	err = binary.Read(buffer, binary.BigEndian, &partLength)
-	if err != nil {
-		return Header{}, err
-	}
-	return Header{partType, partLength}, nil
-}
-
-func ParseStringPart(header Header, buffer *bytes.Buffer) (part interface{}, err error) {
-	contentBytes := buffer.Next(int(header.Length - 4))
-	//Trim the null terminating byte from the string
-	content := string(contentBytes[0 : len(contentBytes)-1])
-	return StringPart{header, content}, nil
-}
-
-func ParseNumericPart(header Header, buffer *bytes.Buffer) (part interface{}, err error) {
-	var content int64
-	err = binary.Read(buffer, binary.BigEndian, &content)
-	if err != nil {
-		return NumericPart{}, err
-	}
-	return NumericPart{header, content}, nil
-}
-
-func ParseHighDefNumericPart(header Header, buffer *bytes.Buffer) (part interface{}, err error) {
-	parsedPart, err := ParseNumericPart(header, buffer)
-	numericPart := parsedPart.(NumericPart)
-	if err != nil {
-		return NumericPart{}, err
-	}
-	return NumericPart{numericPart.Header, numericPart.Content >> 30}, nil
-}
-
-func parseParts(buffer *bytes.Buffer) []interface{} {
-	var partsSlice []interface{}
-	for buffer.Len() > 0 {
-		header, err := ParseHeader(buffer)
-		if err != nil {
-			fmt.Errorf("err encountered %v", err)
-		}
-		if parser, ok := parsers[header.Type]; ok {
-			part, err := parser(header, buffer)
-			if err != nil {
-				fmt.Errorf("err encountered %v", err)
-			}
-			partsSlice = append(partsSlice, part)
-		} else {
-			buffer.Next(int(header.Length - 4))
-		}
-	}
-	return partsSlice
 }
 
 func FindStringParts(partTypeId uint16, parts []interface{}) (stringParts []StringPart, err error) {
